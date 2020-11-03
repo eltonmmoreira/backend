029184f679f7681bf2a88d87431b52a3e03c0056
eltonmmoreira <eltonmmoreira@gmail.com>
2020-11-03 08:18:56 -0300

    Validações

--- src/main/java/br/com/pessoa/api/pessoa/Pessoa.java
+++ src/main/java/br/com/pessoa/api/pessoa/Pessoa.java
@@ -9,7 +9,6 @@ import lombok.ToString;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -19,10 +18,7 @@ import java.time.LocalDate;
 @Setter
 @ToString(of = {"nome", "cpf"})
 @Entity
-@Table(name = "PESSOA",
-    uniqueConstraints = @UniqueConstraint(
-            columnNames = {"CPF", "STATUS"})
-)
+@Table(name = "PESSOA")
 public class Pessoa extends BaseEntity {
 
     @NotNull(message = "{nome.notnull}")
